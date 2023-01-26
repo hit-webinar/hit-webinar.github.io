6f18c34b524ad7ecfea9418db1eb4e106ec4526a
duducheng <jekyll4168@icloud.com>
2023-01-26 18:57:37 +0100

    added CNAME

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # hit-webinar.github.io
 
-Source of the Hit Webinar homepage.
-
 HIT网络研讨会项目主页。
+
+Source of the HIT Webinar homepage.
+
---
+++ assets/data.js
@@ -0,0 +1,21 @@
+const site = {
+    year: "2022-2023",
+    repoURL: "https://github.com/hit-webinar/hit-webinar.github.io",
+    siteURL: "https://hit-webinar.com/",
+    creator: "Jiancheng Yang",
+    creatorURL: "https://jiancheng-yang.com/",
+    showPageviews: false,
+};
+
+const about = `HIT Webinar是一个聚焦生物医疗，人工智能以及前沿科技的网络研讨会。<br>
+HlT意为Healthcare，Intelligence和Technology，关注诸多医疗科技领域的前沿问题，包括医学影像处理、计算机视觉、智慧医疗、生物信息学、新型临床诊疗装备、智能康复机器人、医疗可穿戴设备、人工智能辅助制药等等。<br>
+HIT Webinar由点内科技、高校学生学术联盟医工学人、上海交通大学HealthX医疗科技俱乐部、DrugAI公众号、中美生科青年创投俱乐部共同发起和组织，包含常规活动和特邀报告。
+常规活动主要包括论文精读分享和产学研用行业新闻分享，特邀报告则将邀请嘉宾分享特定领域的前沿进展。<br>
+活动通常于每周五晚上进行。`;
+
+const reports = [
+    {
+        "id": "routine23",
+        "title": "",
+    }
+];
