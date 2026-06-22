2d76e4cc74acd56ae7f48190cbf43f7bc01f5b61
duducheng <jekyll4168@icloud.com>
2026-06-22 14:05:06 +0300

    add talk260613 video replay

--- assets/data.js
+++ assets/data.js
@@ -31,7 +31,7 @@ const reports = [
             },
             {
                 tag: "视频回放",
-                // href: "https://www.bilibili.com/video/#TODO"
+                href: "https://www.bilibili.com/video/BV1XZ7N6SEmQ/"
             },
         ],
         link: {
